06f2b7728b9f882767d4bbfa0404739c47f321f8
jays-visionAI <jays@visai.io>
2026-01-30 09:08:56 +0900

    deploy: Vision Bridge to Testnet v2
    Contracts deployed:
    - IntentCommitment: 0x47c05BCCA7d57c87083EB4e586007530eE4539e9
    - MessageInbox: 0x408F924BAEC71cC3968614Cb2c58E155A35e6890
    - ChallengeManager: 0x773330693cb7d5D233348E25809770A32483A940
    - VisionEqualizerV3: 0x52173b6ac069619c206b9A0e75609fC92860AB2A

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -38,7 +38,8 @@ module.exports = {
             url: "https://api.visionchain.co/rpc-proxy",
             chainId: 1337,
             accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"], // Admin Key
-            gasPrice: "auto"
+            gasPrice: 1000000000, // 1 gwei
+            gas: 8000000
         },
         // Ethereum Sepolia Testnet
         sepolia: {
