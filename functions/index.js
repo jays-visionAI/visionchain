c01c7388674545a43fbf61095f60950997f62a77
jays-visionAI <jays@visai.io>
2026-02-09 17:20:58 +0900

    fix: add approve to VCN_TOKEN_ABI for staking contract approval

--- functions/index.js
+++ functions/index.js
@@ -510,6 +510,7 @@ const VCN_TOKEN_ABI = [
   "function transferFrom(address from, address to, uint256 amount) external returns (bool)",
   "function balanceOf(address account) external view returns (uint256)",
   "function allowance(address owner, address spender) external view returns (uint256)",
+  "function approve(address spender, uint256 amount) external returns (bool)",
   "function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external",
 ];
 
