af39c333358c6bf5d8277bb5e0c8befeedb7b42c
jays-visionAI <jays@visai.io>
2026-02-10 10:21:11 +0900

    fix: auto-fix remaining block-spacing lint errors

--- functions/index.js
+++ functions/index.js
@@ -5477,21 +5477,21 @@ exports.weeklyActivityReport = onSchedule({
             if (notif.amount) {
               try {
                 totalStakedWei += BigInt(notif.amount);
-              } catch (e) {/* ignore parse errors */ }
+              } catch (e) {/* ignore parse errors */}
             }
           } else if (notifType.includes("unstake")) {
             stakingActions++;
             if (notif.amount) {
               try {
                 totalUnstakedWei += BigInt(notif.amount);
-              } catch (e) {/* ignore */ }
+              } catch (e) {/* ignore */}
             }
           } else if (notifType.includes("claim") || notifType.includes("reward")) {
             stakingActions++;
             if (notif.amount) {
               try {
                 rewardsClaimedWei += BigInt(notif.amount);
-              } catch (e) {/* ignore */ }
+              } catch (e) {/* ignore */}
             }
           }
         }
@@ -5515,7 +5515,7 @@ exports.weeklyActivityReport = onSchedule({
               if (bData.amount) {
                 try {
                   bridgeVolumeWei += BigInt(bData.amount);
-                } catch (e) {/* ignore */ }
+                } catch (e) {/* ignore */}
               }
             }
           }
